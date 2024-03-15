59f6a0536f22cb4b08a1ab10409d8289ed87079e
Claudia Davila <claudiagdaz@gmail.com>
2024-03-14 19:13:29 -0600

    Crop images in game cards
    Dynamically modify background_img url so it shows a smaller version of the image.
    Makes the loading easier.

--- game_hub/src/components/GameCard.tsx
+++ game_hub/src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../services/image-url";
 
 export interface GameCardProps {
   game: Game;
@@ -10,7 +11,7 @@ export interface GameCardProps {
 const GameCard = ({ game }: GameCardProps) => {
   return (
     <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
         <HStack justifyContent='space-between'>
