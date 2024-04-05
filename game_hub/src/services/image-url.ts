71b099284bced8dfb3a12b288162d58ef38496da
Claudia Davila <claudiagdaz@gmail.com>
2024-04-05 14:18:42 -0600

    Fixed issue with game cards with no imageURL provided

--- game_hub/src/services/image-url.ts
+++ game_hub/src/services/image-url.ts
@@ -1,10 +1,22 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp"
+
 const getCroppedImageUrl = (url: string) => {
-    if (!url) return '';
-    const cropImgUrl = 'https://media.rawg.io/media/crop/600/400'
-    const index = url.indexOf('games/')
-    const updatedImageUrl = cropImgUrl +'/'+ url.slice(index);
+    if (!url) return noImage;
+    const searchTerm = 'media/';
+    const indexOfSearchTerm= url.indexOf(searchTerm) + searchTerm.length;
+    const baseUrl = url.slice(0, indexOfSearchTerm);
+    const urlKey = url.slice(indexOfSearchTerm, url.length)
+    const cropInstruction = "crop/600/400/"
+
+    const updatedImageUrl = baseUrl + cropInstruction + urlKey
+
+
     return updatedImageUrl
 }
 
 
-export default getCroppedImageUrl
+//note some images are baseURL/media/games/key.jpg
+//others are baseURL/media/screenshots/key.jpg sooo current cropped image doesn't work for them
+//Need to rewrite code to be dependent on /media/ 
+
+export default getCroppedImageUrl;
